25f8dd3bd3cb8de9f143bf8733629c123bd8fc8e
Adam Cooke <adam@atechmedia.com>
2013-12-12 20:29:30 +0000

    more styles

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,11 +7,8 @@
     "use strict";
 
     $(document).ready(function(){
-
-        // On the home page, move the blog icon inside the header 
-        // for better relative/absolute positioning.
-
-        //$("#blog-logo").prependTo("#site-head-content");
+	$("a[href^='http://']").attr("target","_blank");
+	$("a[href^='https://']").attr("target","_blank");
 
     });
 
